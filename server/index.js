6139935811e86adb3a64e5f9ecd8b2e4cd9b9b23
Jeremy Stein <stei0805@umn.edu>
2021-07-29 13:47:39 -0400

    Initialized project

--- server/index.js
+++ server/index.js
@@ -1,5 +1,53 @@
-import express from "express";
+/*
+This file contains the main express server logic for the project. It will handle
+API requests for the app and interface with the database.
+*/
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 
+// Load .env variables into process environment.
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB.
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    // Simple console logging.
+    console.log("Connecting to database...")
+    if (err) {
+        // Color codes can be found at:
+        // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+        console.log("\x1b[31m%s\x1b[0m", "Something went wrong when connecting to the database:")
+        console.log(err)
+    } else {
+        console.log("\x1b[32m%s\x1b[0m", "Successfully connected to database.")
+    }
+});
+
+
+// Initialize express app.
 const app = express();
 
+
+// Load in middleware
+app.use(express.json()); // Enable JSON requests and responses.
+app.use(express.urlencoded({extended: true}));
+
+
+// CRUD API router middleware
+/* Empty rn */
+app.use("/", require("./routes/users"));
+
+
+// Listen on a port.
+app.listen(PORT, (err) => {
+    // Simple console logging.
+    console.log("Starting server...")
+    if (err) {
+        console.log("\x1b[31m%s\x1b[0m", "Something went wrong when starting the server:");
+        console.log(err);
+    } else {
+        console.log("\x1b[32m%s\x1b[0m", `Listening on port: ${PORT}`);
+    }
+})
